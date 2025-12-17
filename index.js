a07387edc8e0b3144eb6873504807347908e41aa
AliFicoTempo <ali.setraprabaperkasa@gmail.com>
2025-12-17 14:49:02 +0700

    fix table and sheet mapping

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function sync() {
   const { rows } = await pool.query(`
     SELECT *
     FROM shipment
-    ORDER BY created_at DESC
+    ORDER BY tanggal DESC
   `);
 
   console.log("TOTAL ROWS:", rows.length);
